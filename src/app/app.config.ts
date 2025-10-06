809d35ac931013af869f3c18931177221598914f
RicardoMA172 <98186789+RicardoMA172@users.noreply.github.com>
2025-10-05 22:36:04 -0600

    Agregar configuración de activos y proveedor de HTTP en la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    // Provide the HTTP client
+    provideHttpClient(),
+    importProvidersFrom(RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }))
+    
   ]
 };
+
+
+
+
+
+    
